4d2fd44aa6d0049fe25638bd5c1fe4ead4df33a7
MengYX <admin@mengyx.cn>
2020-11-05 17:49:12 +0800

    [P2PNG-25] Add comments for plugin

--- plugin.go
+++ plugin.go
@@ -8,10 +8,14 @@ func init() {
 	plugins = make(map[string]RouterPlugin)
 }
 
+// RegisterRouterPlugin is used to register a plugin
+// Notice: should called by package init()
 func RegisterRouterPlugin(plugin RouterPlugin) {
 	x := plugin.PluginInfo()
 	plugins[x.Name] = plugin
 }
+
+// GetRouterPluginRegistry returns a list of all registered RouterPlugin
 func GetRouterPluginRegistry() []RouterPlugin {
 	x := make([]RouterPlugin, len(plugins))
 	i := 0
@@ -22,12 +26,16 @@ func GetRouterPluginRegistry() []RouterPlugin {
 	return x
 }
 
-// Get a Router Plugin from registry by key
+// GetRouterPlugin return a RouterPlugin from registry that matches the key
 func GetRouterPlugin(name string) (p RouterPlugin, ok bool) {
 	p, ok = plugins[name]
 	return
 }
 
+// RouterPlugin is a type that is used as a p2pNG plugin.
+// p2pNG will call PluginInfo first, so PluginInfo should returns static info,
+// then fill the Config and call Init,
+// at last GetRouter() to mount and init database buckets.
 type RouterPlugin interface {
 	PluginInfo() *PluginInfo
 	GetRouter() chi.Router
@@ -35,6 +43,10 @@ type RouterPlugin interface {
 	Config() interface{}
 }
 
+// PluginInfo is a type that describe a p2pNG plugin.
+// Name is should be unique, usually use module path for it.
+// Prefix indicates the http service path prefix.
+// Buckets indicates which database bucket will use.
 type PluginInfo struct {
 	Name    string
 	Version string
